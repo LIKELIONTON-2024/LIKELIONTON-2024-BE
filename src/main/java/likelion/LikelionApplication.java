ae825b01354187b18a5dc1dd29e3ba7e1e237a98
mangsuyo <mangsuyo3610@gmail.com>
2024-07-21 23:19:18 +0900

    init: first commit with boiler-plate

---
+++ src/main/java/likelion/LikelionApplication.java
@@ -0,0 +1,13 @@
+package likelion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikelionApplication.class, args);
+		System.out.println("HELLO WORLD!");
+	}
+}
---
+++ src/test/java/likelion/LikelionApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
