b500947fd35fcc107acc636e533b44d0db5412fd
che7371 <che7371@gmail.com>
2024-07-24 06:19:10 +0900

    feat: 친구 삭제

--- src/main/java/likelion/friend/FriendController.java
+++ src/main/java/likelion/friend/FriendController.java
@@ -1,6 +1,8 @@
 package likelion.friend;
 
+import jakarta.persistence.EntityNotFoundException;
 import likelion.user.User;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,6 +28,18 @@ public class FriendController {
     public ResponseEntity<List<User>> searchUserNickname(@RequestParam String searchKeyword){
         List<User> searchFriendList=friendService.getUsersBySearchKeyword(searchKeyword);
         return ResponseEntity.ok(searchFriendList);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> deleteFriendFromList(@RequestParam Long friendId, @RequestParam Long userId){
+        try {
+            friendService.deleteFriendFromList(friendId, userId);
+            return ResponseEntity.ok("Friend removed successfully.");
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while removing the friend.");
+        }
     }
 
 }
--- src/main/java/likelion/friend/FriendService.java
+++ src/main/java/likelion/friend/FriendService.java
@@ -1,9 +1,9 @@
 package likelion.friend;
 
+import jakarta.persistence.EntityNotFoundException;
 import likelion.user.User;
 import likelion.user.UserRepository;
 import org.springframework.stereotype.Service;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,6 +27,17 @@ public class FriendService {
 
     public List<User> getUsersBySearchKeyword(String searchKeyword){
         return userRepository.findByNicknameIsContaining(searchKeyword);
+    }
+
+    public void deleteFriendFromList(Long friendId,Long userId){
+        User user=userRepository.findById(userId).orElseThrow(() -> new EntityNotFoundException("User not found"));
+        User friend=userRepository.findById(friendId).orElseThrow(()->new EntityNotFoundException("Friend not found"));
+
+        Friend friendRelation1 = friendRepository.findByUserAndFriend(user, friend).orElseThrow(() -> new EntityNotFoundException("Friend relationship not found"));
+        Friend friendRelation2 = friendRepository.findByUserAndFriend(friend, user).orElseThrow(() -> new EntityNotFoundException("Friend relationship not found"));
+
+        friendRepository.delete(friendRelation1);
+        friendRepository.delete(friendRelation2);
     }
 
 }
