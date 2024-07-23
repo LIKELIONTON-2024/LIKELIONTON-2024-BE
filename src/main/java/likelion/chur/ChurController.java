ea69151ea28d5887c1805e2d5c27021db6cb1d2c
che7371 <che7371@gmail.com>
2024-07-24 06:33:07 +0900

    feat: 츄르 보내기

--- src/main/java/likelion/chur/ChurController.java
+++ src/main/java/likelion/chur/ChurController.java
@@ -1,6 +1,10 @@
 package likelion.chur;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +15,16 @@ public class ChurController {
 
     public ChurController(ChurService churService) {
         this.churService = churService;
+    }
+
+    @PostMapping("/send")
+    public ResponseEntity<String> sendChur(@RequestParam Long senderId, @RequestParam Long receiverId) {
+        boolean isSent = churService.sendChur(senderId, receiverId);
+        if (isSent) {
+            return ResponseEntity.ok("Chur sent successfully.");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed to send Chur. Already sent today.");
+        }
     }
 
 }
