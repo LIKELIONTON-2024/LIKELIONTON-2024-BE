00f52d9e6e414521aa19e642f611c5cc7b3dfc77
mangsuyo <mangsuyo3610@gmail.com>
2024-07-26 17:22:32 +0900

    chore: 환경 변수 지정 코드 수정

--- src/main/java/likelion/LikelionApplication.java
+++ src/main/java/likelion/LikelionApplication.java
@@ -5,13 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class LikelionApplication {
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-		
 		SpringApplication.run(LikelionApplication.class, args);
+
+		Dotenv dotenv = Dotenv.configure().directory("./LIKELIONTON-2024-BE").load();
+
+		for (DotenvEntry entry : dotenv.entries()) {
+			System.setProperty(entry.getKey(), entry.getValue());
+		}
 	}
 }
