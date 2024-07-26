639004f3bf9cb32a330cafc1cd62eb1fa13c4a5c
mangsuyo <mangsuyo3610@gmail.com>
2024-07-26 17:15:15 +0900

    deploy: 배포시 환경변수 설정

--- src/main/java/likelion/LikelionApplication.java
+++ src/main/java/likelion/LikelionApplication.java
@@ -4,9 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class LikelionApplication {
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		
 		SpringApplication.run(LikelionApplication.class, args);
 	}
 }
